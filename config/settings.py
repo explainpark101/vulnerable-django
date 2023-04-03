aa72d6eed4974a65532e3fe3138328410e4dab4b
박재형 <explainpark101@gmail.com>
2023-04-03 14:24:15 +0900

    DEBUG off

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i_2mspq_u=m%dnqq-^--7xz2&qp$m&(qq7&22)w#+!w!sbvfrc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
